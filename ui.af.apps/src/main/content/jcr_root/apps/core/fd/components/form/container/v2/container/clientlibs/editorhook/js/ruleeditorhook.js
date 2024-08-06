6aeded9c73566284e7ed2ed6f048ddd234bd65c2
Arun Kumar Attri <aattri@adobe.com>
2024-08-06 16:32:47 +0530

    FT_FORMS-14068

--- ui.af.apps/src/main/content/jcr_root/apps/core/fd/components/form/container/v2/container/clientlibs/editorhook/js/ruleeditorhook.js
+++ ui.af.apps/src/main/content/jcr_root/apps/core/fd/components/form/container/v2/container/clientlibs/editorhook/js/ruleeditorhook.js
@@ -34,9 +34,21 @@
         if (!formContainerPath) {
             showAlert();
         } else {
-            // let ruleEditorUri = '/aem/af/expeditor.html' + getFormContainerPath(editable) + "?fieldPath=" + editable.path + "&fieldId=" + getFieldId(editable);
-            let ruleEditorUri = '/ui/solutions/livecycle-ruleeditor-ui-service/static-assets/index.html?formpath=' + getFormContainerPath(editable) + "&fieldpath=" + editable.path + "&fieldid=" + getFieldId(editable);
-            ruleEditorFrame.setAttribute('src', ruleEditorUri);
+            let ruleEditorUri = '/aem/af/expeditor.html' + getFormContainerPath(editable) + "?fieldPath=" + editable.path + "&fieldId=" + getFieldId(editable);
+            if (true || Granite.Toggles.isEnabled("FT_FORMS-14068")) {
+                const lang = document.documentElement.lang || 'en';
+                if (window.isOriginatorUnifiedShellEnabled) { // author in unified-shell then load spa using /ui
+                    ruleEditorUri = '/ui/solutions/livecycle-ruleeditor-ui-service/static-assets/index.html?formpath=' + getFormContainerPath(editable) + "&fieldpath=" + editable.path + "&fieldid=" + getFieldId(editable) + "&lang=" + lang;
+                    ruleEditorFrame.setAttribute('src', ruleEditorUri);
+                } else { // if author is not using unified shell - local setup/admin login instead of IMS then get markup and assets from spa and replace innerhtml
+                    // The spa-html-repload route provides the CORS header to HTML files, that's how Unified Shell creates srcDoc
+                    ruleEditorUri = '/solutions/livecycle-ruleeditor-ui-service/spa-html-preload/index.html';
+                    const ruleEditorContent = _openRuleEditorFromSPA(ruleEditorUri, getFormContainerPath(editable), editable.path, getFieldId(editable), lang);
+                    ruleEditorFrame.setAttribute('srcdoc', ruleEditorContent);
+                }
+            } else {
+                ruleEditorFrame.setAttribute('src', ruleEditorUri);
+            }
             ruleEditorFrame.setAttribute('title', 'AF Rule Editor');
             ruleEditorFrame.style.display = "block";
             ruleEditorFrame.style.width = "100%";
@@ -47,6 +59,55 @@
             ruleEditorFrame.style.zIndex = "10";
             document.body.appendChild(ruleEditorFrame);
         }
+    }
+
+    function _openRuleEditorFromSPA(ruleEditorUri, formPath, selectedFieldPath, fieldDefinitionId, lang) {
+        //TODO: Make it prod:https://experience.adobe.com before release
+        const experienceEndPoint = 'https://experience-stage.adobe.com';
+        const fetchUrl =`${experienceEndPoint}${ruleEditorUri}`;
+        console.debug('fetchUrl', fetchUrl);
+        const fetchedContent = getContent(fetchUrl);
+
+        let htmlDom = new DOMParser().parseFromString(fetchedContent, 'text/html');
+        const base = document.createElement('base');
+        base.href = window.location.origin;
+        document.getElementsByTagName('head')[0].appendChild(base);
+
+        const ruleMetaInfoElement = document.createElement('div');
+        ruleMetaInfoElement.setAttribute('id', 'rule-meta-info');
+        ruleMetaInfoElement.setAttribute('data-formpath', formPath);
+        ruleMetaInfoElement.setAttribute('data-fieldpath', selectedFieldPath);
+        fieldDefinitionId && ruleMetaInfoElement.setAttribute('data-fieldid', fieldDefinitionId);
+        htmlDom.getElementById('rule-meta-info') && htmlDom.getElementById('rule-meta-info').replaceWith(ruleMetaInfoElement);
+        htmlDom.documentElement.lang = lang;
+
+        let serializer = new XMLSerializer();
+        let htmlString = serializer.serializeToString(htmlDom);
+        return htmlString;
+    }
+
+    function getContent(fetchUrl, dataType) {
+        let content;
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', fetchUrl, false);  // false makes it synchronous
+
+        try {
+            xhr.send();
+            if (xhr.status !== 200) {
+                throw new Error(`Response status not ok: ${xhr.status}`);
+            }
+
+            const responseText = xhr.responseText;
+            if (dataType && dataType.toLowerCase() === 'json') {
+                content = JSON.parse(responseText);
+            } else {
+                content = responseText;
+            }
+        } catch (error) {
+            console.error('Error fetching content:', error);
+        }
+
+        return content;
     }
 
     function getFormContainerPath(editable) {
